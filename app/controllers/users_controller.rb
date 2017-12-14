fae82615385f7603c5b54a83e193e470949f7a5c
Omkar Ghag <omkarghag@gmail.com>
2017-12-14 05:21:55 +0000

    Added code Sign in directly on sign up without having to log in again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
    # render plain: params[:user].inspect #see what data gets passed
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id #added so that user doesn't have to log in again after signup
       flash[:success] = "Welcome to the Alpha Blog  #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
